626fe0830550c6aff97cb84c73e3b4e68d81f0fd
tangym <tangym@mytable.net>
2022-09-06 10:07:31 +0800

    修改样例说明

--- src/config_sample.php
+++ src/config_sample.php
@@ -8,11 +8,11 @@ $config = array (
 	//商户密钥
 	'secret_key' => "",
 	
-	//异步通知地址
-	'notify_url' => "http://shouyinsdk.airpayee.com/webpay/notify_url.php",
+	//异步通知地址，需修改为开发者的地址
+	'notify_url' => "http://paysdk.airpayee.com/wappay/notify_url.php",
 	
-	//同步跳转
-	'return_url' => "http://shouyinsdk.airpayee.com/webpay/return_url.php",
+	//同步跳转，需修改为开发者的地址
+	'return_url' => "http://paysdk.airpayee.com/wappay/return_url.php",
 
 	//编码格式，定值无需修改
 	'charset' => "UTF-8"
