6b42d8f601e3c1be8acb776bcf1a7028ee587788
tangym <tangym@mytable.net>
2022-09-06 10:44:19 +0800

    修改回调地址

--- src/config_sample.php
+++ src/config_sample.php
@@ -9,10 +9,10 @@ $config = array (
 	'secret_key' => "",
 	
 	//异步通知地址，需修改为开发者的地址
-	'notify_url' => "http://paysdk.airpayee.com/wappay/notify_url.php",
+	'notify_url' => "http://paysdk.airpayee.com/webpay/notify_url.php",
 	
 	//同步跳转，需修改为开发者的地址
-	'return_url' => "http://paysdk.airpayee.com/wappay/return_url.php",
+	'return_url' => "http://paysdk.airpayee.com/webpay/return_url.php",
 
 	//编码格式，定值无需修改
 	'charset' => "UTF-8"
